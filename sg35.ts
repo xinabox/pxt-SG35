79bb980312671d5412830237a0c7a3ab03b43942
luqmaanb <lb@xinabox.cc>
2020-07-06 00:26:40 +0200

    Initial commit

---
+++ sg35.ts
@@ -0,0 +1,61 @@
+//%color=#444444 icon="\uf07b"
+namespace SG35 {
+
+    let initialized = false
+    let onReceivedDataHandler: (pm1: number, pm25: number, pm10: number) => void;
+
+    //%shim=sg35::begin
+    function begin(): void {
+        return
+    }
+
+    //%shim=sg35::read
+    export function read(): boolean {
+        return true
+    }
+
+
+    //%shim=sg35::pm1
+    export function pm1(): number {
+        return 1
+    }
+
+
+    //%shim=sg35::pm25
+    export function pm25(): number {
+        return 1
+    }
+
+
+    //%shim=sg35::pm10
+    export function pm10(): number {
+        return 1
+    }
+
+    //%shim=sg35::onDataReceived
+    function onDataReceived(body: Action): void {
+        return;
+    }
+
+    function init() {
+        if (initialized) return;
+        initialized = true;
+
+        onDataReceived(() => {
+            let rcv = read()
+            if (rcv) {
+                onReceivedDataHandler(pm1(), pm25(), pm10())
+            }
+            rcv = false
+        })
+    }
+
+    //% block="SG35 on received "
+    //% draggableParameters=reporter
+    export function onReceivedData(cb: (receivedPM1: number,receivedPM25: number,receivedPM10: number) => void): void {
+        init();
+        onReceivedDataHandler = cb
+    }
+
+    begin()
+}
